fb0957ee8e384eb2909076c67a4ef6e2129fbe6a
Katarina Grujic <katarina.grujic97@gmail.com>
2019-04-19 09:18:05 +0200

    Passing Unknown Props in HOC

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.module.css';
-
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
     render(){
@@ -20,4 +20,4 @@ class Person extends Component {
    
 };
 
-export default Person;
+export default withClass(Person, classes.Person);
